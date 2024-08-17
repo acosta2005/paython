58f0ca146f39fb91717838b0cc32190b4567ef04
acosta2005 <kellybenites761@gmail.com>
2024-08-17 01:58:55 +0000

    gg

---
+++ calificar_1.py
@@ -0,0 +1,11 @@
+puntaje = int(input("ingresar puntos del 1 al 100"))
+
+if puntaje >= 90 and puntaje <= 100:
+    print("calificacion: A")
+elif puntaje >= 80 and puntaje < 90:
+    print("calificion: 8")
+elif puntaje >= 70 and puntaje < 70:
+    prin("calificacion: D")
+elif puntaje >= 60 and puntaje < 70:
+else:
+    print("calificacion: F"p)
