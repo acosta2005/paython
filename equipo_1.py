20e019fb7991ad8a9aa9fe7f1dbfbfedd1caea71
acosta2005 <kellybenites761@gmail.com>
2024-08-20 00:03:59 +0000

    equipo 1

---
+++ equipo_1.py
@@ -0,0 +1,12 @@
+jugador = input("ingresar un jugador de futbol ")
+
+if jugador == "Vidal":
+    print("colo colo")
+elif jugador == "Matador":
+    print("La U ")
+elif jugador == "Palacios":
+    print("colo colo")
+elif jugador == "Falcon":
+    print("colo colo")
+else:
+    print("quien")
