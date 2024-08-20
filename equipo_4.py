94746d3af40c4f7c20150926e0aea14c1d5da893
acosta2005 <kellybenites761@gmail.com>
2024-08-20 00:44:47 +0000

    equipo 4

---
+++ equipo_4.py
@@ -0,0 +1,10 @@
+jugador = input("ingresar un jugador de futbol: ")
+
+match jugador:
+    case "vidal" | "palacios" | "falcon":
+        print("colo colo")
+    case "matador":
+        print("la U")
+    Case _:
+        print(quien")
+              
