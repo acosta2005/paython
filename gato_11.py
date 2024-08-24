f585496ccda459307f9ca36007e849a8eb133510
acosta2005 <kellybenites761@gmail.com>
2024-08-24 02:04:01 +0000

    numeros de gatos

---
+++ gato_11.py
@@ -0,0 +1,15 @@
+def main():
+    while True:
+    n = int(input("ingresa un valor n mayor que 0 "))
+    if n < 0:
+        continue
+    else:
+        break
+miau(n)
+
+
+def miau(n):
+    for i in range(n):
+        print("miau")
+
+        main()
