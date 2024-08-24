dd49f10dc1a5f155031a2cda501e3cdb021a2295
acosta2005 <kellybenites761@gmail.com>
2024-08-24 00:54:20 +0000

    1

--- calificar_1.py
+++ calificar_1.py
@@ -1,11 +1,13 @@
-puntaje = int(input("ingresar puntos del 1 al 100"))
+puntaje = int(input("ingresar puntos del 1 al 100:  "))
 
 if puntaje >= 90 and puntaje <= 100:
     print("calificacion: A")
-elif puntaje >= 80 and puntaje < 90:
-    print("calificion: 8")
-elif puntaje >= 70 and puntaje < 70:
-    prin("calificacion: D")
-elif puntaje >= 60 and puntaje < 70:
+elif puntaje >=80 and puntaje < 90:
+    print("calificacion: B")
+elif puntaje >=70 and puntaje < 80:
+    print("calificacion: C")
+elif puntaje >=60 and puntaje < 70:
+    print("calificacion: D")
+
 else:
-    print("calificacion: F"p)
+    print("calificacion: F")
